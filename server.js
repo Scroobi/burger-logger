9917195f83f9e902fce7c11ac81c60c52a8bc7a3
bryce <blrippey@gmail.com>
2019-05-29 14:17:28 -0700

    final

---
+++ server.js
@@ -0,0 +1,21 @@
+let express = require("express");
+let bodyParser = require("body-parser");
+let exphbs = require("express-handlebars");
+
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({defaultlayout: "main"}));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/burgers_controllers.js");
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("server running on port: " + PORT);
+});
